b0b9a6e26fc137b6615faa003c6ab5aeeb702587
ezmoney2104 <48146739+ezmoney2104@users.noreply.github.com>
2024-11-05 22:38:46 +0800

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <RegistrationForm />
+    <LoginForm />
+  </div>
+</template>
+
+<script>
+import RegistrationForm from './components/RegistrationForm.vue'
+import LoginForm from './components/LoginForm.vue'
+
+export default {
+  components: {
+    RegistrationForm,
+    LoginForm
+  }
+}
+</script>
+
+<style>
+/* Add your styles here */
+</style>
