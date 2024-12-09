e8d3c5d01807edb38982b118a535134a67ae5644
ezmoney2104 <48146739+ezmoney2104@users.noreply.github.com>
2024-12-09 23:34:10 +0800

    Update HomeView.vue

--- HomeView.vue
+++ HomeView.vue
@@ -58,3 +58,92 @@ export default {
   },
 }
 </script>
+
+
+-----
+
+import { shallowMount } from '@vue/test-utils'
+import HomeView from '@/components/HomeView.vue'
+import CommonLabel from '@/components/common/CommonLabel.vue'
+import { TITLELABEL } from '@/common/recipe/rCommonLabel'
+
+describe('HomeView.vue', () => {
+  let wrapper
+  let mockDate
+
+  beforeEach(() => {
+    jest.useFakeTimers() // Mock setInterval
+    mockDate = new Date('2024-12-09T12:34:56') // Fixed date for testing
+    jest.setSystemTime(mockDate) // Mock the system time
+    wrapper = shallowMount(HomeView, {
+      stubs: {
+        CommonLabel: true, // Stub CommonLabel component
+      },
+    })
+  })
+
+  afterEach(() => {
+    jest.clearAllTimers()
+    jest.useRealTimers()
+  })
+
+  it('should be a Vue instance', () => {
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  it('should render the correct number of app bar titles', () => {
+    const labels = wrapper.findAll('.v-app-bar-title')
+    expect(labels.length).toBe(TITLELABEL.length)
+    labels.wrappers.forEach((labelWrapper, index) => {
+      expect(labelWrapper.text()).toBe(TITLELABEL[index].display.label)
+    })
+  })
+
+  it('should render the correct formatted date in the header', () => {
+    const dateElement = wrapper.find('h4 span')
+    expect(dateElement.text()).toBe(
+      mockDate.toLocaleString('ja-JP', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+      })
+    )
+  })
+
+  it('should have the correct initial data', () => {
+    expect(wrapper.vm.mainLabel).toEqual(TITLELABEL)
+    expect(wrapper.vm.currentDate).toEqual(mockDate)
+  })
+
+  it('should update the currentDate every second', () => {
+    const initialDate = wrapper.vm.currentDate
+    jest.advanceTimersByTime(1000)
+    expect(wrapper.vm.currentDate).not.toEqual(initialDate)
+  })
+
+  it('should format the current date correctly', () => {
+    const formattedDate = wrapper.vm.formattedDate
+    expect(formattedDate).toBe(
+      mockDate.toLocaleString('ja-JP', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+      })
+    )
+  })
+
+  it('should clear the interval when component is unmounted', () => {
+    const clearIntervalSpy = jest.spyOn(window, 'clearInterval')
+    wrapper.unmount()
+    expect(clearIntervalSpy).toHaveBeenCalledWith(wrapper.vm.interval)
+  })
+})
+
